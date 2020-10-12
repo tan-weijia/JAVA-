01bdfeb845965ccd11084306f81d72b6ecbeb939
tan-weijia <72741202+tan-weijia@users.noreply.github.com>
2020-10-12 20:29:45 +0800

    Update CPU.java

--- CPU.java
+++ CPU.java
@@ -2,10 +2,17 @@ package computer;
 
 public class CPU {
 	int speed;
+	public String brand; 
 	void setSpeed(int m){
 		speed=m;
 	}
 	public int getSpeed(){
 		return speed;
+	}
+	public String getbrand(){
+		return brand;
+	}
+	public void setbrand(String brand){
+		this.brand=brand;
 	}
 }
