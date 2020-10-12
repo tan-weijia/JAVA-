1e952499d601aec3ace93dd9c9569ea06bd2282f
tan-weijia <72741202+tan-weijia@users.noreply.github.com>
2020-10-12 20:31:56 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -4,8 +4,10 @@ public class Test {
 	public static void main(String[] args){
 		CPU cpu=new CPU();
 		cpu.speed=2200;
+		cpu.brand="华硕";
 		HardDisk  disk=new HardDisk();
-		disk.amount=200;
+		disk.amount = 200;
+		disk.price=2000;
 		PC pc=new PC();
 		pc.setCPU(cpu);
 		pc.setHardDisk(disk);
